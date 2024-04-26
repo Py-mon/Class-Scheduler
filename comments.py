c8f6b7415d9d9615a48261e761e0c7cc82f8175d
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2024-04-25 19:46:16 -0500

    1

---
+++ comments.py
@@ -0,0 +1,16 @@
+# TODO teacher can only teach x amount before break
+
+# ? Will the program be given what days the course should be on? Or should the program be given x amount of days per week and the program decide what days (It might come up with things like Monday,Tues,Friday which doesn't really make a lot of sense compared to Monday, Wednesday, Friday)
+# ? Will each teacher have their own classroom? [Otherwise make list of possible classrooms] Will the data have the classrooms or should the program figure that out?
+
+# Availability of teachers (some are part-time, some full-time, some can only teach mornings, some only afternoons)
+# We may want the option to say that they can't teach more than x hours before they have a prep period/break
+# There are multiple sections of some core courses (like English 9, Bible 10, etc.) while others only have one section - aka ABC
+# Some courses that have a prerequisite (like Photography) need to be offered in an hour when that prerequisite can be completed by a specific grade (9th grade, for example)
+# We may want to select some courses offered in the afternoon while others are only offered in the morning (core courses like English, History, Science, Math are often better for students in the morning when they're fresh rather than after lunch when brain fatigue can set in)
+# Room use needs to be considered as well - for example: the gym can only be available certain hours to make sure elementary can eat lunch and also have time for gym class
+# Teacher schedules need to have a specific number of preps and teaching hours based on their status (full-time vs. part-time)
+# Classes are sometimes offered on different days (for example: M/T/Th v. 5 days a week)
+
+#  Weight given to courses and duties to try to balance staff responsibilities. For example: teaching a class might be weighted as a "3" and doing lunch duty or lobby duty might be rated as a "1" so those teaching less classes may be assigned more lobby duty or study halls, etc.
+#  Additional duties that should be factored into staff schedules (lobby duty, study hall, door duty, lunch duty, etc.
